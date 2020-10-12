9cb3db0b5902c86ef9bed7b40abe1b34d2755c80
Gara64 <paul@cozycloud.cc>
2020-10-12 22:58:20 +0200

    style: Lint

--- MMM-Coinbase.js
+++ MMM-Coinbase.js
@@ -1,111 +1,128 @@
 Module.register("MMM-Coinbase", {
+  defaults: {
+    apiKey: "",
+    apiSecret: "",
+    wallet: ["BTC"],
+    icons: true, // currently only Bitcoin and Ethereum supported
+    label: false,
+    showBalanceInOriginalCurrency: false,
+    updateInterval: 10 // in seconds
+  },
 
-	defaults: {
-		apiKey: "",
-		apiSecret: "",
-		wallet: ["BTC"],
-		icons: true, // currently only Bitcoin and Ethereum supported
-		label: false,
-		showBalanceInOriginalCurrency: false,
-		updateInterval: 10 // in seconds
-	},
+  getStyles: function () {
+    return [this.file("css/styles.css")];
+  },
 
-	getStyles: function() {
-		return [this.file("css/styles.css")];
-	},
+  start: function () {
+    this.icons = {
+      BTC: "bitcoin",
+      ETH: "ethereum"
+    };
+    this.balance = 0;
+    this.currency = "";
+    this.cryptoData = [];
+    this.sendSocketNotification("GET_ACCOUNTS", {
+      apiKey: this.config.apiKey,
+      apiSecret: this.config.apiSecret,
+      wallet: this.config.wallet
+    });
+  },
 
-	start: function() {
-		this.icons = {
-			"BTC": "bitcoin",
-			"ETH": "ethereum"
-		};
-		this.balance = 0;
-		this.currency = "";
-		this.cryptoData = [];
-		this.sendSocketNotification("GET_ACCOUNTS", {apiKey: this.config.apiKey, apiSecret: this.config.apiSecret, wallet: this.config.wallet});
-	},
+  getHeader: function () {
+    return (
+      "<div class='header'><span class='left'>" +
+      this.data.header +
+      "</span><span class='right'>" +
+      this.balance +
+      " " +
+      this.currency +
+      "</span></div>"
+    );
+  },
 
-	getHeader: function() {
-		return "<div class='header'><span class='left'>" + this.data.header + "</span><span class='right'>" + this.balance + " " + this.currency + "</span></div>";
-	},
+  getDom: function () {
+    const elem = document.createElement("div");
+    elem.id = "accountWrapper";
 
-	getDom: function() {
-		const elem = document.createElement("div");
-		elem.id = "accountWrapper";
+    const module = this;
+    const config = this.config;
+    const icons = this.icons;
 
-		const module = this;
-		const config = this.config;
-		const icons = this.icons;
-		
-		if (config.showBalanceInOriginalCurrency) {
-			balanceKey = "balance"
-		} else {
-			balanceKey = "native_balance"
-		}
+    let balanceKey = "balance";
+    if (!config.showBalanceInOriginalCurrency) {
+      balanceKey = "native_balance";
+    }
+    this.cryptoData.forEach((accts) => {
+      module.balance += parseInt(accts.native_balance.amount);
 
-		this.cryptoData.forEach((accts) => {
-			module.balance += parseInt(accts.native_balance.amount);
-			
-			// check if currency is in config
-			if(config.wallet.indexOf(accts.currency) > -1) {
-				// add div ROW for currency
-				const rowElement = document.createElement("div");
-				rowElement.className = "row";
-				rowElement.id = accts.currency + "Counter";
+      // check if currency is in config
+      if (config.wallet.indexOf(accts.currency) > -1) {
+        // add div ROW for currency
+        const rowElement = document.createElement("div");
+        rowElement.className = "row";
+        rowElement.id = accts.currency + "Counter";
 
-				// Create column for icon if icons have been activated
-				if (config.icons) {
-					const columnIconElement = document.createElement("div");
-					columnIconElement.className = "column icon";
+        // Create column for icon if icons have been activated
+        if (config.icons) {
+          const columnIconElement = document.createElement("div");
+          columnIconElement.className = "column icon";
 
-					if(icons.hasOwnProperty(accts.currency))
-						columnIconElement.innerHTML = "<i class='fa fa-" + icons[accts.currency] + "'></i>";
-					else
-						columnIconElement.innerHTML = "<span class='currency-name'>" + accts.currency.toUpperCase() + "</span>";
+          if (icons.hasOwnProperty(accts.currency))
+            columnIconElement.innerHTML =
+              "<i class='fa fa-" + icons[accts.currency] + "'></i>";
+          else
+            columnIconElement.innerHTML =
+              "<span class='currency-name'>" +
+              accts.currency.toUpperCase() +
+              "</span>";
 
-					rowElement.appendChild(columnIconElement);
-				}
+          rowElement.appendChild(columnIconElement);
+        }
 
-				if (config.label) {
-					const columnCurrencyElement = document.createElement("div");
-					columnCurrencyElement.className = "column";
-					columnCurrencyElement.innerHTML = "<span>" + accts.currency + "</span>";
-					rowElement.appendChild(columnCurrencyElement);
-				}
+        if (config.label) {
+          const columnCurrencyElement = document.createElement("div");
+          columnCurrencyElement.className = "column";
+          columnCurrencyElement.innerHTML =
+            "<span>" + accts.currency + "</span>";
+          rowElement.appendChild(columnCurrencyElement);
+        }
 
-				const columnAmountElement = document.createElement("div");
-				columnAmountElement.className = "column";
-				const accountAmount = accts[balanceKey].amount
-				columnAmountElement.innerHTML = "<span> " + accountAmount + "</span>";
+        const columnAmountElement = document.createElement("div");
+        columnAmountElement.className = "column";
+        const accountAmount = accts[balanceKey].amount;
+        columnAmountElement.innerHTML = "<span> " + accountAmount + "</span>";
 
-				rowElement.appendChild(columnAmountElement);
-				elem.appendChild(rowElement);
-			}
+        rowElement.appendChild(columnAmountElement);
+        elem.appendChild(rowElement);
+      }
+    });
 
-		});
+    return elem;
+  },
 
-		return elem;
-	},
+  notificationReceived: function (notification) {
+    switch (notification) {
+      case "DOM_OBJECTS_CREATED":
+        setInterval(() => {
+          this.sendSocketNotification("GET_ACCOUNTS", {
+            apiKey: this.config.apiKey,
+            apiSecret: this.config.apiSecret,
+            wallet: this.config.wallet
+          });
+        }, config.updateInterval * 1000);
 
-	notificationReceived: function(notification) {
-		switch(notification) {
-			case "DOM_OBJECTS_CREATED":
-				setInterval(() => {
-					this.sendSocketNotification("GET_ACCOUNTS", {apiKey: this.config.apiKey, apiSecret: this.config.apiSecret, wallet: this.config.wallet});
-				}, config.updateInterval * 1000);
-				
-				break;
-			}
-	},
+        break;
+    }
+  },
 
-	socketNotificationReceived: function(notification, payload) {
-		switch(notification) {
-			case "ACCOUNTS":
-				this.cryptoData = payload;
-				this.currency = payload[0].native_balance.currency;
-				this.updateDom();
-				this.balance = 0;
-				break;
-		}
-	},
+  socketNotificationReceived: function (notification, payload) {
+    switch (notification) {
+      case "ACCOUNTS":
+        this.cryptoData = payload;
+        this.currency = payload[0].native_balance.currency;
+        this.updateDom();
+        this.balance = 0;
+        break;
+    }
+  }
 });
--- node_helper.js
+++ node_helper.js
@@ -1,21 +1,27 @@
 const NodeHelper = require("node_helper");
-const {Client} = require("coinbase");
+const { Client } = require("coinbase");
 
 module.exports = NodeHelper.create({
-	start: () => {},
-	socketNotificationReceived: function(notification, payload) {
-		const client = new Client({apiKey: payload.apiKey, apiSecret: payload.apiSecret, strictSSL: false});
-		switch(notification) {
-			case "GET_ACCOUNTS":
-				const helper = this;
-				client.getAccounts({}, (err, accounts) => {
-					if(err)
-						throw err;
+  start: () => {},
+  socketNotificationReceived: function (notification, payload) {
+    const client = new Client({
+      apiKey: payload.apiKey,
+      apiSecret: payload.apiSecret,
+      strictSSL: false
+    });
+    switch (notification) {
+      case "GET_ACCOUNTS":
+        const helper = this;
+        client.getAccounts({}, (err, accounts) => {
+          if (err) throw err;
 
-					helper.sendSocketNotification("ACCOUNTS", accounts.filter(a => payload.wallet.includes(a.currency)));
-				});
+          helper.sendSocketNotification(
+            "ACCOUNTS",
+            accounts.filter((a) => payload.wallet.includes(a.currency))
+          );
+        });
 
-				break;
-			}
-	},
+        break;
+    }
+  }
 });
